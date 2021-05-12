881a683bf6db7990c3ab93d774fa1a5a65538ef2
Tobias B <germansuperioz@yahoo.de>
2021-05-12 17:35:25 +0200

    Initial commit

---
+++ src/main/java/network/cow/mc/cowboards/CowboardsPlugin.kt
@@ -0,0 +1,15 @@
+package network.cow.mc.cowboards
+
+import org.bukkit.plugin.java.JavaPlugin
+
+/**
+ * @author Tobias Büser
+ */
+class CowboardsPlugin : JavaPlugin() {
+
+    override fun onEnable() {
+        // do nothing
+    }
+
+}
+
